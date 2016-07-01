1ace950ee61ca7ff2d3885a7ca5040c68b17eaf6
Loenix <florent@sowapps.com>
2016-07-01 11:42:02 +0200

    Update getting started page

--- app/web/themes/default/layouts/app/gettingstarted.php
+++ app/web/themes/default/layouts/app/gettingstarted.php
@@ -11,12 +11,19 @@ Orpheus PHP Framework comes with an easy-to-use installer allowing you to create
 You could also directly download the archive from GitHub.
 </p>
 
-<h2>Use installer</h2>
+<h2>Use the Installer Setup</h2>
 <p>
 This feature is using composer to direct download Orpheus with its dependencies.
 </p>
+<h4>Download the latest installer version</h4>
+<p>First, <a href="<?php echo AppConfig::instance()->get('setup_latest_url'); ?>" target="_blank">download the latest phar of the installer</a> from github.</p>
+<h4>Run the setup</h4>
+<p>
+In the parent folder of your new project, run the setup <code>php orpheus.phar install myprojectname</code>.<br>
+Orpheus &amp; Composer will be installed in a subfolder named myprojectname.
+</p>
 
-<h2>Download</h2>
+<h2>Manual Download</h2>
 <p>
 You could also create a project by yourself, just download directly Orpheus.
 </p>
@@ -24,7 +31,7 @@ You could also create a project by yourself, just download directly Orpheus.
 	<div class="col-sm-4">
 		<h3 id="download-latest">The Latest</h3>
 		<p>An archive of original sources, consider that Composer is not initialized.</p>
-		<p><a href="<?php _u(ROUTE_DOWNLOAD_LATEST); ?>" class="btn btn-lg btn-success">Download Latest</a></p>
+		<p><a href="<?php _u(ROUTE_DOWNLOAD_LATEST); ?>" class="btn btn-lg btn-success" target="_blank">Download Latest</a></p>
 	</div>
 	<div class="col-sm-4">
 		<h3 id="browser-releases">The Releases</h3>
@@ -41,6 +48,6 @@ You could also create a project by yourself, just download directly Orpheus.
 <h1>How to start with Orpheus Framework ?</h1>
 
 <p class="lead">
-The Orpheus Framework is completly customizable but it started with MVC Library, an ORM and all features you need to get an enhanced website.
+The Orpheus Framework is entirely customizable but it started with MVC Library, an ORM and all features you need to get an enhanced website.
 </p>
 
